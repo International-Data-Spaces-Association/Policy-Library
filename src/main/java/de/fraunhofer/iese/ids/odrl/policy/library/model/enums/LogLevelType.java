0180a2f81c6fbeaa3936d287d15f62587d180b0d
Arghavan Hosseinzadeh <arghavan.hosseinzadeh@iese.fraunhofer.de>
2021-04-19 13:31:35 +0200

    Log level Enum is used as notify level enum too.

--- src/main/java/de/fraunhofer/iese/ids/odrl/policy/library/model/enums/LogLevelType.java
+++ src/main/java/de/fraunhofer/iese/ids/odrl/policy/library/model/enums/LogLevelType.java
@@ -10,13 +10,13 @@ public enum LogLevelType {
 
     DEBUG_LEVEL("idsc:DEBUG_LEVEL"),*/
 
-    ON_DENY("idsc:LOG_ON_DENY"),
+    ON_DENY("idsc:ON_DENY"),
 
-    ON_ALLOW("idsc:LOG_ON_ALLOW"),
+    ON_ALLOW("idsc:ON_ALLOW"),
 
-    ON_DUTY_EXERCISED("idsc:LOG_ON_DUTY_EXERCISED"),
+    ON_DUTY_EXERCISED("idsc:ON_DUTY_EXERCISED"),
 
-    ON_ACTION_OPERATED("idsc:LOG_ON_ACTION_OPERATED");
+    ON_ACTION_OPERATED("idsc:ON_ACTION_OPERATED");
 
 
     private final String type;
